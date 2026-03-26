680f47d1e81ccd6ef52c40d470ecd9429c4eb78e
Lucas <lucasabk23@gmail.com>
2026-03-26 14:04:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,12 @@
 import entities.Department;
+import entities.Seller;
+
+import java.sql.Date;
 
 void main(){
     Department obj = new Department(1, "Books");
-    System.out.println(obj);
+
+    Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new DateTimeFormatterBuilder().toFormatter(), 3000.00, obj);
+
+    System.out.println(seller);
 }
