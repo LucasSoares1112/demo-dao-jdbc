696ba294f20f491e149b6dc9bf22162bc6ab4cf8
Lucas <lucasabk23@gmail.com>
2026-04-09 13:28:20 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/entities/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package entities.dao;
+
+import entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer Id);
+    List<Department> findAll();
+}
---
+++ src/entities/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package entities.dao;
+
+import entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer Id);
+    List<Seller> findAll();
+}
